d0ce61a5b2407aeb282ac4e942c4813735c75750
faggqt <faggqt@d2k5.com>
2017-08-12 18:55:30 +0100

    Removed unused and irrelevant js files.

--- syndbb/static/js/auth/auth_upld.js
+++
@@ -1,7 +0,0 @@
-$("#uploadlogin").submit(function() {
-  var crypt = new Crypt();
-  var plain_pw = $("#inputPassword").val();
-  var password = CryptoJS.SHA256(plain_pw);
-
-  $("#pword").val(password);
-})
