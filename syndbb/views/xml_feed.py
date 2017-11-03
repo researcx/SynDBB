0af9d8dd9532a4aeac386700191291456ac903a8
Erika Treial <erika@d2k5.com>
2017-11-03 00:03:55 +0000

    added generalised xml feed

--- syndbb/views/xml_feed.py
+++ syndbb/views/xml_feed.py
@@ -28,6 +28,95 @@ def has_no_empty_params(rule):
     return len(defaults) >= len(arguments)
 
 
+### Non-Forum Specific ###
+
+# Post Feed
+# /feed/posts/xml
+@syndbb.app.route("/feed/posts/xml")
+@syndbb.cache.memoize(timeout=60)
+def feed_posts_xml_all():
+    activity = []
+    activity_item = ""
+    count = 0
+    limit = 10
+
+    posts = d2_activity.query.filter(d2_activity.replyto != 0).order_by(d2_activity.time.desc()).limit(limit).all()
+
+    for post in posts:
+        activity.append([post.time, "post", post.id])
+
+    activity.sort(reverse=True)
+
+    for item in activity:
+        if count < limit:
+            if item[1] == "post":
+                post = d2_activity.query.filter(d2_activity.id == item[2]).first()
+                thread = d2_activity.query.filter(d2_activity.id == post.replyto).first()
+                forum = d2_forums.query.filter(d2_forums.id == thread.category).first()
+
+                if post and thread and forum:
+                    if post.anonymous == 0:
+                        latestreplier = post.user.username
+                    else:
+                        latestreplier = 'Anonymous'
+
+                    activity_item += '''<item>
+                                    		<guid>'''+str(post.id)+'''</guid>
+                                    		<title>'''+latestreplier+''' replied to "'''+html_escape(thread.title)+'''" in '''+html_escape(forum.name)+'''</title>
+                                    		<link>https://d2k5.com/'''+str(forum.short_name)+'''/'''+str(thread.id)+'''#'''+str(post.id)+'''</link>
+                                    		<pubDate>'''+human_date(post.time)+'''</pubDate>
+                                    	</item>'''
+            count += 1
+
+    template = syndbb.render_template('post_feed.xml', posts=activity_item)
+    response = make_response(template)
+    response.headers['Content-Type'] = 'application/xml'
+    return response
+
+# Thread Feed
+# /feed/threads/xml
+@syndbb.app.route("/feed/threads/xml")
+@syndbb.cache.memoize(timeout=60)
+def feed_threads_xml_all():
+    activity = []
+    activity_item = ""
+    count = 0
+    limit = 10
+
+    threads = d2_activity.query.filter(d2_activity.category != 0).order_by(d2_activity.reply_time.desc()).limit(limit).all()
+
+    for thread in threads:
+        activity.append([thread.time, "thread", thread.id])
+
+    activity.sort(reverse=True)
+
+    for item in activity:
+        if count < limit:
+            if item[1] == "thread":
+                thread = d2_activity.query.filter(d2_activity.id == item[2]).first()
+                forum = d2_forums.query.filter(d2_forums.id == thread.category).first()
+
+                if thread and forum:
+                    if thread.anonymous == 0:
+                        threadcreator = thread.user.username
+                    else:
+                        threadcreator = 'Anonymous'
+
+                    activity_item += '''<item>
+                                    		<guid>'''+str(thread.id)+'''</guid>
+                                    		<title>'''+threadcreator+''' created "'''+html_escape(thread.title)+'''" in '''+html_escape(forum.name)+'''</title>
+                                    		<link>https://d2k5.com/'''+str(forum.short_name)+'''/'''+str(thread.id)+'''</link>
+                                    		<pubDate>'''+human_date(thread.time)+'''</pubDate>
+                                    	</item>'''
+            count += 1
+
+    template = syndbb.render_template('thread_feed.xml', threads=activity_item)
+    response = make_response(template)
+    response.headers['Content-Type'] = 'application/xml'
+    return response
+
+### Forum Specific
+
 # Forum List
 # /feed/channels
 @syndbb.app.route("/feed/channels")
